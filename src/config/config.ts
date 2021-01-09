b7ea6746095487cbbcd99fc17d1514c9bc747a05
seamlessOlanrewaju <olanrewaju@seamlesshr.com>
2021-01-10 00:28:30 +0100

    feature/udacity-image-filter-microservice

---
+++ src/config/config.ts
@@ -0,0 +1,21 @@
+export const config = {
+  "postgress": {
+    "username": "olanrewajudev",
+    "password": "olanrewajudev",
+    "database": "olanrewajudev",
+    "host": "olanrewajudev.cdivd2oc1rwt.us-east-1.rds.amazonaws.com",
+    "dialect": "postgress",
+  },
+  "aws": {
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udacitybeanstalkproject"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
